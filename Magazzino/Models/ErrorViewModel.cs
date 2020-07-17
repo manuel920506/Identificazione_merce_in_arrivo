2d40b5bda7c57b047f87b276b81939f70dc20ff8
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2020-07-17 02:52:29 +0200

    Inizio creazione Anagrafica Prodotti Magazzino

---
+++ Magazzino/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Magazzino.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
